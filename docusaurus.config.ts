a12ea2880ca9aa15c1ae84391787f8a6dc1ae7bf
WheheoHu <wheheohu@outlook.com>
2024-09-13 22:26:56 +0800

    update

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -12,7 +12,7 @@ const config: Config = {
   url: 'https://www.github.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/bmd_doc/',
   trailingSlash: false,
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
